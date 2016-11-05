9d64c3532ba881c1194c3e5e3e22ff874cea02e7
yohai <yohigh5@gmail.com>
2016-11-05 02:18:56 +0200

    added notification labels to client UI

--- DarkChessClient/ClientUI.Designer.cs
+++ DarkChessClient/ClientUI.Designer.cs
@@ -34,6 +34,8 @@
             this.XPointerBoardLocationStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
             this.YPointerBoardLocationStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
             this.board = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.menuStrip.SuspendLayout();
             this.pointerBoardLocationStrip.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.board)).BeginInit();
@@ -45,7 +47,7 @@
             this.connectToolStripMenuItem});
             this.menuStrip.Location = new System.Drawing.Point(0, 0);
             this.menuStrip.Name = "menuStrip";
-            this.menuStrip.Size = new System.Drawing.Size(556, 24);
+            this.menuStrip.Size = new System.Drawing.Size(784, 24);
             this.menuStrip.TabIndex = 0;
             this.menuStrip.Text = "menuStrip1";
             // 
@@ -62,7 +64,7 @@
             this.YPointerBoardLocationStatusLabel});
             this.pointerBoardLocationStrip.Location = new System.Drawing.Point(0, 581);
             this.pointerBoardLocationStrip.Name = "pointerBoardLocationStrip";
-            this.pointerBoardLocationStrip.Size = new System.Drawing.Size(556, 22);
+            this.pointerBoardLocationStrip.Size = new System.Drawing.Size(784, 22);
             this.pointerBoardLocationStrip.TabIndex = 1;
             this.pointerBoardLocationStrip.Text = "statusStrip1";
             // 
@@ -90,19 +92,43 @@
             this.board.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.board.TabIndex = 2;
             this.board.TabStop = false;
-            this.board.MouseClick += board_MouseClick;
             // 
-            // DarkChessClientUI
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Location = new System.Drawing.Point(557, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Your turn!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Location = new System.Drawing.Point(556, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(182, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "An enemy pawn has been promoted!";
+            // 
+            // DarkChessClient
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(556, 603);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(784, 603);
+            this.ControlBox = false;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.board);
             this.Controls.Add(this.pointerBoardLocationStrip);
             this.Controls.Add(this.menuStrip);
             this.MainMenuStrip = this.menuStrip;
             this.MaximizeBox = false;
-            this.Name = "DarkChessClientUI";
+            this.Name = "DarkChessClient";
             this.Text = "DarkChess client";
             this.menuStrip.ResumeLayout(false);
             this.menuStrip.PerformLayout();
@@ -124,6 +150,8 @@
         private System.Windows.Forms.PictureBox board;
 
         System.Drawing.Graphics gameBoardGraphics;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
